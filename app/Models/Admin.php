6c08106d10ea804339f1a2469827d53979a3a4de
Lone-Wolf17 <53178226+Lone-Wolf17@users.noreply.github.com>
2020-12-14 19:12:07 +0100

    ** Modify Admin Model for JWT implementation

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -5,12 +5,38 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Admin extends Model
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Admin extends Authenticatable implements JWTSubject
 {
+    use Notifiable;
+
     use HasFactory;
 
     protected $table = 'admins';
     protected $fillable = ['name', 'email', 'password', 'created_at', 'updated_at'];
     protected $hidden = ['password', 'remember_token'];
 
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
